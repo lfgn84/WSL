0c09b69b56d2cd8666cf99693113f21fad1d6a98
Luis <lfgn84@gmail.com>
2020-02-16 20:58:52 +0100

    fixing dBparser

--- Server/src/main/java/server/Controller.java
+++ Server/src/main/java/server/Controller.java
@@ -20,11 +20,13 @@ public class Controller {
     private String DEFAULT_FILE ;
     private String FILE_NOT_FOUND ;
     private DBparser dBparser;
+    private Properties prop;
     private long counter;
     public Controller(Request request, Response response,Properties prop)  {
         this.request = request;
         this.response = response;
         this.counter=0;
+        this.prop=prop;
         WEB_ROOT = new File(prop.getProperty("WSL.StaticFilesRoot"));
         pl =new PluginSearcher(prop.getProperty("WSL.Pluginroot"));
         FILE_NOT_FOUND=prop.getProperty("WSL.FILE_NOT_FOUND");
@@ -38,14 +40,20 @@ public class Controller {
             response.setContentType("text/html");
             response.setResponseCode("200 ok");
         }
+        else if(request.getMethod().equals("POST")){
+            Postprocess();
+            Getprocess();
+        }
     }
-
+    private void Postprocess() throws IOException{
+        counter++;
+        dBparser = new DBparser(request.headers.get(13),counter,prop,response);
+    }
     private void Getprocess() throws IOException {
 
 
 
-        counter++;
-        dBparser = new DBparser(request.headers.get(13),counter);
+
 
         if(request.fileRequested.equals("/")){
             request.fileRequested="/"+DEFAULT_FILE;
--- Server/src/main/java/server/DBparser.java
+++ Server/src/main/java/server/DBparser.java
@@ -1,5 +1,6 @@
 package server;
 
+import Spi.Response;
 import com.mongodb.Block;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -9,13 +10,31 @@ import com.mongodb.client.model.*;
 import org.bson.Document;
 import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Projections.*;
+
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class DBparser {
 
-    public DBparser (String toParse, long counter) {
+    public DBparser (String toParse, long counter, Properties prop, Response response) throws IOException {
+        ProcessBuilder pb = new ProcessBuilder();
+        ProcessBuilder pb1 = new ProcessBuilder();
+        pb.command(prop.getProperty("WSL.mongodbpath")+"mongod");
+        pb1.command(prop.getProperty("WSL.mongopath")+"mongo");
+        Process p;
+        Process m;
+
+        p = pb.start();
+        m = pb1.start();
+        System.out.println(p.isAlive());
+        System.out.println(m.isAlive());
+//            p.destroyForcibly();
+//            m.destroyForcibly();
+
+
         List<Document> documents = new ArrayList<Document>(); // Creating a Document ArrayList named "documents" where we will save our documents.
         String y = toParse.substring(toParse.lastIndexOf(toParse));
         String z = (y.replace("&"," ").replace("%40","@"));
@@ -31,13 +50,13 @@ public class DBparser {
         Block<Document> printBlock = new Block<Document>() { // Creating a "printBlock" method that will identify and print out our documents on blocks in Json format.
             @Override
             public void apply(final Document document) {
-                System.out.println(document.toJson());
+                response.setBody(document.toJson());
             }
         };
 
 
-        Document doc1 = new Document("_id", counter)   //Creating our new documents and appending their fields and values for our collection("restaurants").
-                .append("name", name)                   //With other words, creating our database of "restaurants" and their details on our collection.
+        Document doc1 = new Document   //Creating our new documents and appending their fields and values for our collection("restaurants").
+                 ("name", name)                   //With other words, creating our database of "restaurants" and their details on our collection.
                 .append("e-mail", email)
                 .append("comment",comment);
 
@@ -51,5 +70,7 @@ public class DBparser {
         coll.find().forEach(printBlock); // Printing all block of documents in  our collection using "find()"  mongodb command.
         System.out.println("");
 
+        p.destroyForcibly();
+        m.destroyForcibly();
     }
 }
